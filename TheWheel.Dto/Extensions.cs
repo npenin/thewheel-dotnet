99945830c3fecac7652d2a743ad8cfc16bd5d7b2
npenin <nicolas.penin@dragon-angel.fr>
2014-09-11 15:52:42 +0200

    added extension method to build gridview results from queryable

---
+++ TheWheel.Dto/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheWheel.Dto
+{
+    public static class Extensions
+    {
+        public static DataTableView ToGridData<T>(IQueryable<T> source, int startRowIndex, int maximumRows)
+        {
+            return new DataTableView()
+            {
+                Count = source.Count(),
+                Data = source.Skip(startRowIndex).Take(maximumRows)
+            };
+        }
+    }
+}
