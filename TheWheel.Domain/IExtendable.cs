1f1d8be24392c872596856fcf1d356d0c6365843
npenin <nicolas.penin@dragon-angel.fr>
2014-10-21 11:21:05 +0200

    restored helpers

--- TheWheel.Domain/IExtendable.cs
+++ TheWheel.Domain/IExtendable.cs
@@ -15,4 +15,22 @@ namespace TheWheel.Domain
         ICollection<T> Properties { get; set; }
         T this[string key] { get; set; }
     }
+
+    public static partial class Extensions
+    {
+        public static IEnumerable<T> Find<T>(this IExtendable<T> extendable, string key)
+            where T : INameable
+        {
+            if (extendable == null)
+                return Enumerable.Empty<T>();
+            else
+                return extendable.Properties.Where(p => p.Name == key);
+        }
+
+        public static T Get<T>(this IExtendable<T> extendable, string key)
+            where T : INameable
+        {
+            return extendable.Find<T>(key).FirstOrDefault();
+        }
+    }
 }
