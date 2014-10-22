259b586e8af0f74629111ef77dd049371c9ae742
npenin <nicolas.penin@dragon-angel.fr>
2014-10-22 12:03:55 +0200

    documentation and trying nuspec

--- TheWheel.Domain/ITrackable.cs
+++ TheWheel.Domain/ITrackable.cs
@@ -5,9 +5,24 @@ using System.Text;
 
 namespace TheWheel.Domain
 {
+    /// <summary>
+    /// Represents an entity having information about its change tracking dates.
+    /// </summary>
     public interface ITrackable
     {
+        /// <summary>
+        /// Gets or sets the date when this instance was created.
+        /// </summary>
+        /// <value>
+        /// the date when this instance was created.
+        /// </value>
         public DateTime CreatedOn { get; set; }
+        /// <summary>
+        /// Gets or sets the date when this instance was updated.
+        /// </summary>
+        /// <value>
+        /// the date when this instance was updated.
+        /// </value>
         public DateTime UpdatedOn { get; set; }
     }
 }
