1212e9f1986148ca7f477987f8d025e33dc4bc66
npenin <nicolas.penin@dragon-angel.fr>
2014-11-03 12:14:49 +0100

    made extension method as extension method

--- TheWheel.Dto/Extensions.cs
+++ TheWheel.Dto/Extensions.cs
@@ -7,7 +7,7 @@ namespace TheWheel.Dto
 {
     public static class Extensions
     {
-        public static DataTableView ToGridData<T>(IQueryable<T> source, int startRowIndex, int maximumRows)
+        public static DataTableView ToGridData<T>(this IQueryable<T> source, int startRowIndex, int maximumRows)
         {
             return new DataTableView()
             {
