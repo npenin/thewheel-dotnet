a0820d9ca72a656e2aad3802a5221c956d8ceb6e
n.penin <n.penin@cdm-trials.com>
2015-06-08 17:03:37 +0200

    made it compile under vs < 2015

--- TheWheel.ServiceBus/SchedulerClient.cs
+++ TheWheel.ServiceBus/SchedulerClient.cs
@@ -74,7 +74,7 @@ namespace TheWheel.ServiceBus
             if (stop)
                 return;
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-            Task.Delay(interval).ContinueWith(Process);
+            Task.Delay(interval).ContinueWith<object>(Process);
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             if (processing)
                 return;
