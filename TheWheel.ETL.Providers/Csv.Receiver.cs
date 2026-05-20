363cd716e64699a6d8fc4d20d62665c8db97de60
npenin <github@dragon-angel.fr>
2026-05-20 08:44:23 +0200

    fix: refactor CsvReceiver to streamline reading and writing logic

--- TheWheel.ETL.Providers/Csv.Receiver.cs
+++ TheWheel.ETL.Providers/Csv.Receiver.cs
@@ -38,112 +38,106 @@ namespace TheWheel.ETL.Providers
             if (options.Separator == Separator.Guess)
                 throw new NotSupportedException("Guess separator is not supported when writing");
             var separatorChar = Csv.GetSeparatorChar(options.Separator);
-            using (var reader = await provider.ExecuteReaderAsync(token))
+            using var reader = await provider.ExecuteReaderAsync(token);
+            using var targetStream = await receiverTransport.GetStreamAsync(token);
+            using var writer = new StreamWriter(targetStream, options.Encoding ?? Encoding.UTF8);
+            var hasRecord = reader.Read();
+            if (options.SkipLines != null)
             {
-                using (var targetStream = await receiverTransport.GetStreamAsync(token))
+                for (var i = 0; i < options.SkipLines.Length; i++)
                 {
-                    using (var writer = new StreamWriter(targetStream, options.Encoding ?? Encoding.UTF8))
+                    await writer.WriteAsync(options.SkipLines[i]);
+                    writer.Write('\n');
+                }
+            }
+            if (options.FirstLineHeader && hasRecord)
+            {
+                for (var i = 0; i < reader.FieldCount; i++)
+                {
+                    if (i > 0)
+                        await writer.WriteAsync(separatorChar);
+
+                    await writer.WriteAsync('"');
+                    await writer.WriteAsync(reader.GetName(i).Replace(@"""", @""""""));
+                    await writer.WriteAsync('"');
+                }
+            }
+            if (hasRecord)
+                do
+                {
+                    writer.Write('\n');
+                    for (var i = 0; i < reader.FieldCount; i++)
                     {
-                        var hasRecord = reader.Read();
-                        if (options.SkipLines != null)
-                        {
-                            for (var i = 0; i < options.SkipLines.Length; i++)
-                            {
-                                await writer.WriteAsync(options.SkipLines[i]);
-                                writer.Write('\n');
-                            }
-                        }
-                        if (options.FirstLineHeader && hasRecord)
-                        {
-                            for (var i = 0; i < reader.FieldCount; i++)
-                            {
-                                if (i > 0)
-                                    await writer.WriteAsync(separatorChar);
+                        if (i > 0)
+                            await writer.WriteAsync(separatorChar);
 
-                                await writer.WriteAsync('"');
-                                await writer.WriteAsync(reader.GetName(i).Replace(@"""", @""""""));
-                                await writer.WriteAsync('"');
-                            }
-                        }
-                        if (hasRecord)
-                            do
+                        if (options.formatters != null && options.formatters[i] != null)
+                            await writer.WriteAsync(options.formatters[i](reader.GetValue(i)));
+                        else
+                        {
+                            switch (Type.GetTypeCode(reader.GetFieldType(i)))
                             {
-                                writer.Write('\n');
-                                for (var i = 0; i < reader.FieldCount; i++)
-                                {
-                                    if (i > 0)
-                                        await writer.WriteAsync(separatorChar);
-
-                                    if (options.formatters != null && options.formatters[i] != null)
-                                        await writer.WriteAsync(options.formatters[i](reader.GetValue(i)));
+                                case TypeCode.Empty:
+                                case TypeCode.DBNull:
+                                    break;
+                                case TypeCode.Object:
+                                    await writer.WriteAsync('"');
+                                    var value = reader.GetValue(i);
+                                    if (value != null && value != DBNull.Value)
+                                        await writer.WriteAsync(value.ToString().Replace(@"""", @""""""));
+                                    await writer.WriteAsync('"');
+                                    break;
+                                case TypeCode.Boolean:
+                                    await writer.WriteAsync(reader.GetBoolean(i).ToString());
+                                    break;
+                                case TypeCode.Char:
+                                    if (reader.GetChar(i) == '"')
+                                        await writer.WriteAsync(@"""""");
                                     else
-                                    {
-                                        switch (Type.GetTypeCode(reader.GetFieldType(i)))
-                                        {
-                                            case TypeCode.Empty:
-                                            case TypeCode.DBNull:
-                                                break;
-                                            case TypeCode.Object:
-                                                await writer.WriteAsync('"');
-                                                var value = reader.GetValue(i);
-                                                if (value != null && value != DBNull.Value)
-                                                    await writer.WriteAsync(value.ToString().Replace(@"""", @""""""));
-                                                await writer.WriteAsync('"');
-                                                break;
-                                            case TypeCode.Boolean:
-                                                await writer.WriteAsync(reader.GetBoolean(i).ToString());
-                                                break;
-                                            case TypeCode.Char:
-                                                if (reader.GetChar(i) == '"')
-                                                    await writer.WriteAsync(@"""""");
-                                                else
-                                                    await writer.WriteAsync(reader.GetChar(i));
-                                                break;
-                                            case TypeCode.Byte:
-                                                writer.Write(reader.GetByte(i));
-                                                break;
-                                            case TypeCode.SByte:
-                                            case TypeCode.Int16:
-                                                writer.Write(reader.GetInt16(i));
-                                                break;
-                                            case TypeCode.UInt16:
-                                            case TypeCode.Int32:
-                                                writer.Write(reader.GetInt32(i));
-                                                break;
-                                            case TypeCode.UInt32:
-                                            case TypeCode.Int64:
-                                            case TypeCode.UInt64:
-                                                writer.Write(reader.GetInt64(i));
-                                                break;
-                                            case TypeCode.Single:
-                                                writer.Write(reader.GetFloat(i));
-                                                break;
-                                            case TypeCode.Double:
-                                                writer.Write(reader.GetDouble(i));
-                                                break;
-                                            case TypeCode.Decimal:
-                                                writer.Write(reader.GetDecimal(i));
-                                                break;
-                                            case TypeCode.DateTime:
-                                                await writer.WriteAsync(reader.GetDateTime(i).ToString("u"));
-                                                break;
-                                            case TypeCode.String:
-                                                await writer.WriteAsync('"');
-                                                await writer.WriteAsync(reader.GetString(i).Replace(@"""", @""""""));
-                                                await writer.WriteAsync('"');
-                                                break;
-                                        }
-                                    }
-                                }
+                                        await writer.WriteAsync(reader.GetChar(i));
+                                    break;
+                                case TypeCode.Byte:
+                                    writer.Write(reader.GetByte(i));
+                                    break;
+                                case TypeCode.SByte:
+                                case TypeCode.Int16:
+                                    writer.Write(reader.GetInt16(i));
+                                    break;
+                                case TypeCode.UInt16:
+                                case TypeCode.Int32:
+                                    writer.Write(reader.GetInt32(i));
+                                    break;
+                                case TypeCode.UInt32:
+                                case TypeCode.Int64:
+                                case TypeCode.UInt64:
+                                    writer.Write(reader.GetInt64(i));
+                                    break;
+                                case TypeCode.Single:
+                                    writer.Write(reader.GetFloat(i));
+                                    break;
+                                case TypeCode.Double:
+                                    writer.Write(reader.GetDouble(i));
+                                    break;
+                                case TypeCode.Decimal:
+                                    writer.Write(reader.GetDecimal(i));
+                                    break;
+                                case TypeCode.DateTime:
+                                    await writer.WriteAsync(reader.GetDateTime(i).ToString("u"));
+                                    break;
+                                case TypeCode.String:
+                                    await writer.WriteAsync('"');
+                                    await writer.WriteAsync(reader.GetString(i).Replace(@"""", @""""""));
+                                    await writer.WriteAsync('"');
+                                    break;
                             }
-                            while (reader.Read() && !token.IsCancellationRequested);
+                        }
                     }
+                }
+                while (reader.Read() && !token.IsCancellationRequested);
 
 #if !NET5_0_OR_GREATER
                     targetStream.Close();
 #endif
-                }
-            }
         }
     }
 
